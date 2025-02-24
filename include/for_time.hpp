15e9f810109610205a2bade7611fe70659d03ae8
saehan Lee/SLAM in the hazardous environments <46841767+himhan34@users.noreply.github.com>
2025-02-24 15:30:43 +0900

    Update for_time.hpp

--- include/for_time.hpp
+++ include/for_time.hpp
@@ -1,23 +1,29 @@
 #ifndef _LIDAR_100_UTILS_TIME_H_
 #define _LIDAR_100_UTILS_TIME_H_
 
-#include <chrono>
-// time counter
+#include <chrono>  // 시간 측정을 위한 헤더 파일
+
+// 실행 시간 측정을 위한 클래스
 class TicToc {
 public:
     TicToc() {
-        tic();
+        tic();  // 객체 생성 시 자동으로 시작 시간 기록
     }
+
+    // 시작 시간 기록
     void tic() {
         start = std::chrono::system_clock::now();
     }
 
-    double toc(){
-        end = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        return elapsed_seconds.count() * 1000;
+    // 경과 시간(ms) 반환
+    double toc() {
+        end = std::chrono::system_clock::now();  // 현재 시간 기록
+        std::chrono::duration<double> elapsed_seconds = end - start;  // 경과 시간 계산
+        return elapsed_seconds.count() * 1000;  // 밀리초 단위 변환 후 반환
     }
+
 private:
-    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::time_point<std::chrono::system_clock> start, end;  // 시작 및 종료 시간 저장 변수
 };
-#endif
+
+#endif  // _LIDAR_100_UTILS_TIME_H_
